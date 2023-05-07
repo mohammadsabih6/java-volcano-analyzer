a2baa88d1c2754de6940fcec54b38549b4bb4d38
mohammadsabih6 <sabih.tariq@xloopdigital.com>
2023-05-08 01:42:56 +0500

    All test done

--- src/main/java/VolcanoAnalyzer.java
+++ src/main/java/VolcanoAnalyzer.java
@@ -28,14 +28,17 @@ public class VolcanoAnalyzer {
         return volcanos.size();
     }
     //All test Methods
+    
     //Q1  Return the volcanoes that erupted in the 1980s.
     public List<Volcano> eruptedInEighties() {
         return volcanos.stream().filter(i -> i.getYear()>=1980 && i.getYear()<1990).collect(Collectors.toList());
     }
+    
     // Q2 Return an array of the names of volcanoes that had an eruption with a Volcanic Explosivity Index (VEI) of 6 or higher.
     public String[] highVEI() {
         return volcanos.stream().filter(i -> i.getVEI() >= 6 ).map(Volcano::getName).collect(Collectors.toList()).toArray(new String[0]);
     }
+    
     // Q3 Return the eruption with the highest number of recorded deaths.
     public Volcano mostDeadly() {
         return volcanos.stream().filter(i -> i.getYear() == 450
@@ -53,10 +56,12 @@ public class VolcanoAnalyzer {
                 .findFirst()
                 .orElse(null);
     }
+    
     // Q4 Return the percentage of eruptions that caused tsunamis.
     public double causedTsunami() {
         return volcanos.stream().filter(i -> i.getTsu().equals("tsu")).count() * 100 / volcanos.size();
     }
+    
     //Q5 Return the most common type of volcano in the set.
     public String mostCommonType() {
         for (Volcano volcano : volcanos) {
@@ -66,34 +71,41 @@ public class VolcanoAnalyzer {
         }
         return null;
     }
+    
     //Q6 Return the number of eruptions when supplied a country as an argument
     public int eruptionsByCountry(String country) {
         return (int) volcanos.stream().filter(i -> i.getCountry().equals(country)).count();
     }
+    
     //Q7 Return the average elevation of all eruptions.
     public double averageElevation() {
         return volcanos.stream().mapToDouble(Volcano::getElevation).average().orElse(0);
     }
+    
     //Q8 Return an array of types of volcanoes.
     public String[] volcanoTypes() {
         return volcanos.stream().map(Volcano::getType).distinct().collect(Collectors.toList()).toArray(new String[0]);
     }
+    
     //Q9 Return the percentage of eruptions that occurred in the Northern Hemisphere.
     public double percentNorth() {      
         return volcanos.stream().filter(i -> i.getLatitude()>0).count() * 100 / volcanos.size();
     }
+    
     //Q10 Return the names of eruptions that occurred after 1800, that did NOT cause a tsunami, happened in the Southern Hemisphere, and had a VEI of 5.
     public String[] manyFilters() {
         return volcanos.stream().filter(i -> i.getYear() > 1800 && i.getVEI() == 5  && i.getLatitude()<0)
         .map(Volcano::getName).collect(Collectors.toList()).toArray(new String[0]);
     }
+    
     //Q11 Return the names of eruptions that occurred at or above an elevation passed in as an argument.
     public String[] loadVolcanoes(int volcano) {
         return volcanos.stream().filter( i -> i.getElevation()> volcano ).map(Volcano::getName).collect(Collectors.toList()).toArray(new String[0]);
     }
+    
     //Q12 Return the agents of death for the ten most deadly eruptions.
      //This code help taken from internet
-     
+
     public String[] topAgentsOfDeath() {
         return volcanos.stream()
             .filter(i -> !i.getDEATHS().isEmpty()).sorted((i1, i2) -> Integer.parseInt(i2.getDEATHS()) - Integer.parseInt(i1.getDEATHS()))
